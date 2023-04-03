ad9b1a241edfdb1b984198a243ac3c85d197bae2
atitiya <gift.22110@gmail.com>
2023-04-03 10:38:19 +0700

    f

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿class Program{
+    
+    public static void Main (string[] args) {
+
+        double[]values = new double[1000];
+        int count = 0;
+        double sum = 0;
+
+        while (true){
+            string input = Console.ReadLine();
+            if (input == "End"){
+                break;
+            }
+        double value = double.Parse(input);
+        count++;
+        sum += value;
+        }
+        
+        string mode = Console.ReadLine();
+
+        switch (mode){
+            case "FindMax";
+            double max = double.MinValue;
+            for (int i = 0;i < count; i==){
+                if (values[i]>max){
+                    max = values[i];
+                }
+            }
+            Console.writeLine(min);
+            bake;
+            case "FindMean";
+            double mean = sum / count;
+            Console.WriteLine(mean);
+            break;
+
+        default;
+        Console.WriteLine("Invalid mode");
+        break;
+        
+        }
+    }
+}
